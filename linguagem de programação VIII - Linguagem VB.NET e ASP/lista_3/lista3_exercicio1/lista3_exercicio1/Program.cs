d76631246a3d7801525db826d03ca39e9886f93a
jvitorxsantos <116177071+jvitorxsantos@users.noreply.github.com>
2022-10-19 20:58:49 -0300

    Add files via upload

---
+++ linguagem de programação VIII - Linguagem VB.NET e ASP/lista_3/lista3_exercicio1/lista3_exercicio1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3_exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            int nota1, nota2,media;
+
+            Console.Write("Digite o nome do aluno: ");          nome = Console.ReadLine();
+
+            Console.Write("Digite a primeira nota: ");          nota1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite a segunda nota: ");           nota2 = int.Parse(Console.ReadLine());
+
+            media = calcularMedia(nota1, nota2);
+            lerNota(media,nome);
+
+            Console.ReadKey();
+
+
+        }
+
+        static int calcularMedia(int x, int z)
+        {
+            int med;
+            med = (x + z) / 2;
+            return med;
+        }
+
+        static void lerNota(double x, String no)
+        {
+            if (x >= 7)
+            {
+                Console.Write("Aluno {0} aprovado!", no);
+            }
+            else
+            {
+                Console.Write("Aluno {0} reprovado!", no);
+            }
+
+        }
+    }
+}
---
+++ linguagem de programação VIII - Linguagem VB.NET e ASP/lista_3/lista3_exercicio2/lista3_exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3_exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, fatorial;
+            Console.Write("Digite um numero para fazer o fatorial dele: ");
+            numero = int.Parse(Console.ReadLine());
+
+            fatorial= calcularFatorial(numero);
+            Console.Write(fatorial);
+            Console.ReadKey();
+        }
+        static int calcularFatorial(int x)
+        {
+            int i,fat=1;
+            for (i=1;i<=x;i++)
+            {
+                fat = fat * i;
+            }
+            return fat;
+        }
+    }
+}
