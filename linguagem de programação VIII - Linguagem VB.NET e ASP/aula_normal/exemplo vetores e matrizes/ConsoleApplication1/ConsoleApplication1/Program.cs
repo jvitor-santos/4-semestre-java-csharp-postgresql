e3ea1dd491272c8eecd0882b94f3772976c4e941
jvitorxsantos <116177071+jvitorxsantos@users.noreply.github.com>
2022-10-19 21:04:59 -0300

    Add files via upload

---
+++ linguagem de programação VIII - Linguagem VB.NET e ASP/aula_normal/exemplo vetores e matrizes/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra;
+            int i;
+
+            Console.Write("Digite uma palavra: ");
+            palavra = Console.ReadLine().ToLower();
+
+            Console.WriteLine("\nVogais da palavra: {0} \n", palavra);
+
+            char[] vetor = palavra.ToCharArray();
+
+            int tamanho = vetor.Length;
+
+            for (i=0; i<tamanho; i++)
+            {
+                if (vetor[i] == 'a' || vetor[i] == 'e' || vetor[i] == 'i' || vetor[i] == 'o' || vetor[i] == 'u')
+                    Console.Write(" {0}", vetor[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
