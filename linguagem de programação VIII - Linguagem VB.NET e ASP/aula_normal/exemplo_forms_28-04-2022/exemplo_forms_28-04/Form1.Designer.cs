561d60a166bdbd5f295cd6008ce5276e1af76332
jvitorxsantos <116177071+jvitorxsantos@users.noreply.github.com>
2022-10-19 21:04:18 -0300

    Add files via upload

---
+++ linguagem de programação VIII - Linguagem VB.NET e ASP/aula_normal/exemplo_forms_28-04-2022/exemplo_forms_28-04/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace exemplo_forms_28_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.lblIgual = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.cmbOper = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor1.Location = new System.Drawing.Point(12, 12);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(48, 24);
+            this.txtValor1.TabIndex = 0;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor2.Location = new System.Drawing.Point(193, 12);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(49, 24);
+            this.txtValor2.TabIndex = 2;
+            // 
+            // lblIgual
+            // 
+            this.lblIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIgual.Location = new System.Drawing.Point(272, 10);
+            this.lblIgual.Name = "lblIgual";
+            this.lblIgual.Size = new System.Drawing.Size(31, 23);
+            this.lblIgual.TabIndex = 3;
+            this.lblIgual.Text = "=";
+            this.lblIgual.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblIgual.Click += new System.EventHandler(this.lblIgual_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(350, 15);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 18);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(12, 153);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 32);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(167, 153);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 32);
+            this.btnOk.TabIndex = 6;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(287, 153);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 32);
+            this.btnSair.TabIndex = 7;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // cmbOper
+            // 
+            this.cmbOper.FormattingEnabled = true;
+            this.cmbOper.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "x",
+            "÷",
+            "%"});
+            this.cmbOper.Location = new System.Drawing.Point(103, 12);
+            this.cmbOper.Name = "cmbOper";
+            this.cmbOper.Size = new System.Drawing.Size(45, 21);
+            this.cmbOper.TabIndex = 8;
+            this.cmbOper.Text = "+";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 275);
+            this.Controls.Add(this.cmbOper);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblIgual);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Label lblIgual;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.ComboBox cmbOper;
+    }
+}
+
---
+++ linguagem de programação VIII - Linguagem VB.NET e ASP/aula_normal/exemplo_forms_28-04-2022/exemplo_forms_28-04/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exemplo_forms_28_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblIgual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2, result;
+            char oper;
+            valor1 = double.Parse(txtValor1.Text);
+            valor2 = double.Parse(txtValor2.Text);
+            oper = char.Parse(cmbOper.Text);
+
+            switch (oper)
+            {
+                case '+':
+                    result = valor1 + valor2;
+                    lblResult.Text = Convert.ToString(result);
+                    break;
+                case '-':
+                    result = valor1 - valor2;
+                    lblResult.Text = Convert.ToString(result);
+                    break;
+                case 'x':
+                    result = valor1 * valor2;
+                    lblResult.Text = Convert.ToString(result);
+                    break;
+                case '÷':
+                    if (valor2 != 0)
+                    {
+                        result = valor1 / valor2;
+                        lblResult.Text = Convert.ToString(result);
+                    }else
+                    {
+                        lblResult.Text = "ERRO!";
+                    }
+                        break;
+                case '%':
+                    result = valor1 % valor2;
+                    lblResult.Text = Convert.ToString(result);
+                    break;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtValor1.Text = "";
+            txtValor2.Text = "";
+            lblResult.Text = "";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+           if (MessageBox.Show("Deseja realmente sair?", "sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes);
+            {
+               MessageBox.Show("Obrigado por usar nosso sistema", "Sair", MessageBoxButtons.OK, MessageBoxIcon.Information);
+               Application.Exit();
+            }
+            
+        }
+    }
+}
