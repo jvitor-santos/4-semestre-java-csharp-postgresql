f562a4c51fd4a9d87391dbfdeef2ce25150b2bcd
jvitorxsantos <116177071+jvitorxsantos@users.noreply.github.com>
2022-10-19 21:03:38 -0300

    Add files via upload

---
+++ linguagem de programação VIII - Linguagem VB.NET e ASP/aula_normal/exeplo retorno/exeplo retorno/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exeplo_retorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            digite();
+            a = int.Parse(Console.ReadLine());
+            dobro(a);
+            Console.WriteLine("\n\nPressione alguma tecla: ");
+            Console.ReadKey();
+        }
+
+        static void digite()
+        {
+            Console.Write("Digite um numero: ");
+        }
+
+        static void dobro(int n)
+        {
+            int d = n * 2;
+            Console.WriteLine("Dobro de {0} = {1}",n,d);
+        }
+    }
+}
